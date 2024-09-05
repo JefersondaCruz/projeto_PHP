6c0142a1e5c7f5990101be5cbb9085d2bba90b86
Jeferson da Cruz <jefersondacruz40@gmail.com>
2024-09-04 21:44:44 -0300

    newcontent/php

--- index.php
+++ index.php
@@ -1,36 +1,57 @@
-<?php    
+<?php
 
-$a =readline("1 - logar  \n  2 - registrar: "); 
+// VARIAVEIS
+$log_usuarios = [];
 
-// LOGAR 
-function logar () {
-    global $usario, $password;
-    $usuario = readline ("Digite seu nome de usuario: ");
-    $password = readline ("Digite sua senha: ");
-    
+// Função para fazer login
+function logar() {
+    global $log_usuarios; 
+    $usuario = readline("Digite seu nome de usuario: ");
+    $password = readline("Digite sua senha: ");
+
+
+    // Verificar se o usuário está registrado
+    foreach ($log_usuarios as $user) {
+        if ($user['nomeUsuario'] === $usuario && $user['senha'] === $password) {
+            echo "Login bem-sucedido\n";
+            return true;
+        }
+    }
+    echo "Nome do usuario ou senha inválido \n";
 }
 
-//REGISTRAR
-function registrar() {
-    $array_usuario = [$reg_usuario = readline ("Digite o usuario para ser cadastrado: ")];
-    $reg_password = readline ( "Digite sua senha para ser cadastrada: ");
+// Função para registrar um novo usuário
+function registrar($reg_usuario, $reg_password) {
+    global $log_usuarios; 
+
+    // Verificar se o usuário já está registrado
+    foreach ($log_usuarios as $user) {
+        if ($user['nomeUsuario'] === $reg_usuario) {
+            echo "Usuário já registrado\n";
+            return;
+        }
+    }
+
+    // Adicionar novo usuário ao array
+    $log_usuarios[] = ['nomeUsuario' => $reg_usuario, 'senha' => $reg_password];
+    echo "Usuário registrado com sucesso\n";
 }
 
 
-if ($a == 1) {
+$Opcao_inicial = readline("1 - logar\n2 - registrar\n");
 
+// Processar a escolha do usuário
+if ($Opcao_inicial == 1) {
     logar();
-    echo "Você está logado \n ";
-    
-
-} else if ($a == 2) {
-
-    registrar();
-    echo "Você foi registrado \n";
-    
-
-} else  {
-    echo "Erro, somente 1 e 2 sao escolhas";
+} else if ($Opcao_inicial == 2) {
+    $reg_usuario = readline("Digite o usuário para ser cadastrado: ");
+    $reg_password = readline("Digite sua senha para ser cadastrada: ");
+    registrar($reg_usuario, $reg_password);
+} else {
+    echo "Erro, somente 1 e 2 são escolhas válidas\n";
 }
 
+while ($log_usuarios == true) {
+    
+}
 
